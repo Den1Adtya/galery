c723624b9a3f573f86d4fb3aed26e988ebe2dd55
Den1Adtya <denzkcoach@gmail.com>
2024-02-05 08:03:13 +0700

    gallery

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -7,16 +7,41 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
     <title>Sinterest</title>
+    <link rel="stylesheet" href="{{ asset('style/style.css') }}">
+    <style>
+      
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.header{
+  background-color: #ffffff;
+}
+
+.container{
+  width: 100%;
+  columns: 5;
+  column-gap: 15px;
+  padding: 15px;
+}
+
+img{
+  width: 100%;
+  margin-bottom: 15px;
+  border-radius: 10px;
+}
+</style>
 </head>
 <body>
 
 <div class="bg-white">
-    <header class="absolute inset-x-0 top-0 z-50">
+    <header class="fixed bg-white inset-x-0 top-0 z-50 ">
       <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
           <a href="/Home " class="-m-1.5 p-1.5">
             <span class="sr-only">Sinterest</span>
-            <img class="h-9 w-auto" src="https://i.ibb.co/ft6fWDV/1706269969738.png" alt="snapedit-1705208572334" alt="Sinterest ">
+            <img class="absolute left-5 top-7 h-10 w-auto" src="https://i.ibb.co/ft6fWDV/1706269969738.png"alt="Sinterest ">
           </a>
         </div>
         <div class="flex lg:hidden">
@@ -38,7 +63,7 @@
               <td>
                 <div>
                 <form action="#">
-                  <input type="text" name="search" placeholder="Search..." style="padding: 10px; border: none; border-radius: 30px; width: 650px; background-color: #e2e2e2;">
+                  <input type="text" name="search" placeholder="Search" style="padding: 10px; border: none; border-radius: 30px; width: 650px; background-color: #e2e2e2;">
                 </div>
               </form>
               </td>
@@ -54,12 +79,12 @@
           <img class="h-7 w-auto" src="https://i.ibb.co/HpvMK5B/message-bubble.png" alt="Messages  ">
         </a>
           
-          <span class="sr-only">Notification</span>
-          <img class="h-7 w-auto" src="https://i.ibb.co/S3jWb4V/user-image-with-black-background.png" alt="Notification  ">
+          <span class="sr-only">Account</span>
+          <img class="h-7 w-auto" src="https://i.ibb.co/S3jWb4V/user-image-with-black-background.png" alt="account  ">
         </a>
 
-        <span class="sr-only">Notification</span>
-        <img class="h-8 w-auto" src="https://i.ibb.co/X7dP5hj/down-1.png" alt="Notification  ">
+        <span class="sr-only">Others</span>
+        <img class="h-8 w-auto" src="https://i.ibb.co/X7dP5hj/down-1.png" alt="Others  ">
       </a>
 
       </div>
@@ -107,37 +132,43 @@
     </header>
 
     <main class="mt-20">
-  <div class="grid grid-cols-4 gap-3 px-10">
-    <div class="grid-item grid-row-2 grid-col-2">
-      <img src="https://i.pinimg.com/474x/f0/95/82/f09582994175230186561d1793af7bc5.jpg" class="rounded-2xl">
-    </div>
-    <div class="grid-item grid-row-1 grid-col-1 ">
-      <img src="https://i.pinimg.com/474x/77/8f/54/778f54cf8a472288c30c4685ce791704.jpg" class="rounded-2xl" >
-    </div>
-    <div class="grid-item grid-row-1 grid-col-1 ">
-      <img src="https://i.pinimg.com/474x/c7/ee/aa/c7eeaa39062e932529805de77b1f4a59.jpg" class="rounded-2xl" >
-    </div>
-    <div class="grid-item grid-row-1 grid-col-2 bg-gray-200 p-4 rounded-lg">
-      <img src="image4.jpg" class="w-full h-32 object-cover">
-      <h2 class="text-lg font-bold mt-2">Title 4</h2>
-      <p class="text-gray-700">Description 4</p>
-    </div>
-    <div class="grid-item grid-row-2 grid-col-2">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCLTIE2BMuA4oRGxVT-KWsPvW7WCokWb1KPcJQfXitF2vFbiS2eJmL1Yild9wVBx3cTRs&usqp=CAU" class="rounded-2xl">
-    </div>
-    <div class="grid-item grid-row-2 grid-col-1 bg-gray-200 p-4 rounded-lg">
-      <img src="image5.jpg" class="w-full h-32 object-cover">
-      <h2 class="text-lg font-bold mt-2">Title 5</h2>
-      <p class="text-gray-700">Description 5</p>
-    </div>
-    <div class="grid-item grid-row-2 grid-col-2 bg-gray-200 p-4 rounded-lg">
-      <img src="image6.jpg" class="w-full h-64 object-cover">
-      <h2 class="text-xl font-bold mt-2">Title 6</h2>
-      <p class="text-gray-700">Description 6</p>
-    </div>
-    <div class="grid-item grid-row-2 grid-col-2">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTZqYO4dOYga0x4Ef72ZLie1W_Mato9A7Ibg&usqp=CAU" class="rounded-2xl">
-    </div>
+  <div class="container">
+      <img src="https://i.pinimg.com/474x/f0/95/82/f09582994175230186561d1793af7bc5.jpg">
+      <img src="https://i.pinimg.com/474x/77/8f/54/778f54cf8a472288c30c4685ce791704.jpg">
+      <img src="https://i.pinimg.com/474x/c7/ee/aa/c7eeaa39062e932529805de77b1f4a59.jpg">
+      <img src="https://i.pinimg.com/474x/6c/25/b0/6c25b037c577fc51472546dc384390e2.jpg">
+      <img src="https://i.pinimg.com/474x/d7/4a/77/d74a77466a51201d1dba2f267630f948.jpg">
+      <img src="https://i.pinimg.com/474x/6e/ef/12/6eef12181e9bc7ab2ec8925f3b8f6760.jpg">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTZqYO4dOYga0x4Ef72ZLie1W_Mato9A7Ibg&usqp=CAU">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK5HXsZGhKU0qYvJWtZl2QwAi6Jzf--Bcw6g&usqp=CAU">
+      <img src="https://i.pinimg.com/474x/f1/c2/5a/f1c25aecafaf87e6ecab2a2704139714.jpg">
+      <img src="https://i.pinimg.com/474x/de/30/82/de3082808c048812071215eb3e4f48ec.jpg">
+      <img src="https://i.pinimg.com/474x/e1/36/37/e1363742238dd33bbc7a26f35efe2717.jpg">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaTPjKEd5MhJ7pD-nKSxa_CCTHw22-GknR3w&usqp=CAU">
+      <img src="https://i.ibb.co/Xp2dt4B/download-2.png">
+      <img src="https://i.pinimg.com/474x/a5/48/d4/a548d4f2f7baa2eb5cc5db29879eb73f.jpg">
+      <img src="https://i.pinimg.com/474x/5f/17/44/5f1744696261c3ebca5f0ea485d93d47.jpg">
+      <img src="https://i.pinimg.com/474x/28/88/c9/2888c9abc725b6561746c125138cacb8.jpg">
+      <img src="https://i.pinimg.com/474x/78/98/11/789811fbace1c2a984d2e7c5f7e6eab5.jpg">
+      <img src="https://i.ibb.co/DKdSvYn/Whats-App-Image-2024-01-30-at-18-55-49-26d090b5.jpg">
+      <img src="https://i.ibb.co/ZVWhDm4/Whats-App-Image-2024-01-30-at-18-55-49-473132b0.jpg">
+      <img src="https://i.ibb.co/nbLX8WC/Whats-App-Image-2024-01-30-at-18-55-49-9aeab52c.jpg">
+      <img src="https://i.pinimg.com/474x/82/30/3e/82303e3c38025a2562780c7271a0aafd.jpg">
+      <img src="https://i.pinimg.com/474x/f0/a6/8f/f0a68f8808476a9e1f0030279a337c02.jpg">
+      <img src="https://i.pinimg.com/474x/e4/4d/aa/e44daa69e1f10d8d776101975f8f1129.jpg">
+      <img src="https://i.pinimg.com/474x/e3/cd/a0/e3cda02c2c2373a06fdea2c3d4042177.jpg">
+      <img src="https://i.pinimg.com/474x/ec/4a/c8/ec4ac82d5b9f3217d6d7a5f1a9da67ce.jpg">
+      <img src="https://media1.tenor.com/m/JWo08tw_xL4AAAAC/siuuuuuu.gif">
+      <img src="https://i.pinimg.com/474x/c2/d7/71/c2d771d49b1a8ad67e36da11afc4ba1a.jpg">
+      <img src="https://i.pinimg.com/474x/ed/c4/d1/edc4d15288252f81c925cb562f7fb74d.jpg">
+      <img src="https://i.pinimg.com/474x/d4/a8/aa/d4a8aaf192347bab795da62544afb813.jpg">
+      <img src="https://i.pinimg.com/474x/38/4a/d3/384ad3860b313f77192e8b6529ee0cd0.jpg">
+      <img src="https://i.pinimg.com/474x/bf/99/8c/bf998ccf5f3da28019c69a5452031ba4.jpg">
+      <img src="https://i.ibb.co/Lh4mQ4J/Whats-App-Image-2024-01-30-at-18-55-43-460fe632.jpg">
+      <img src="https://i.pinimg.com/originals/ca/e5/35/cae535fb04a99bc1747afd8b0c357e3d.gif">
+      <img src="https://iili.io/Ha1Ew2s.gif">
+      <img src="https://media.tenor.com/htK6C7Jwqk4AAAAM/cantik-sekaleee.gif">
+      <img src="https://i.ibb.co/LR8Kq1Q/Whats-App-Video2024-02-01at14-23-02-093447d5-ezgif-com-video-to-gif-converter.gif">
   </div>
     </main>
 </body>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+  <title>Pinterest Registration</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    usmans
+  <h1>Welcome to Pinterest</h1>
+  <p>Find new ideas to try</p>
+  <form action="/submit_registration" method="post">
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email" required>
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password" required>
+    <label for="birthdate">Birthdate (dd/mm/yyyy)</label>
+    <input type="text" id="birthdate" name="birthdate" required pattern="(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d">
+    <input type="submit" value="Continue">
+    <p>OR</p>
+    <a href="/facebook_login">Continue with Facebook</a>
+    <a href="/google_login">Continue with Google</a>
+    <p>By continuing, you agree to Pinterest's <a href="/terms_of_service">Terms of Service</a> and acknowledge you've read our <a href="/privacy_policy">Privacy Policy</a>.</p>
+    <p>Already a member? <a href="/login">Log in</a></p>
+    <p>Create a free <a href="/business_signup">business account</a>.</p>
+  </form>
 </body>
 </html>
